c68aff3d6ce3d46caff9f1244bef038829fb4251
Jeff Hagadorn <jhagadorn@mgmresorts.com>
2025-02-05 12:59:32 -0800

    Rewrite sudo logic for systems that don't use bash

--- install-zscaler-ca.sh
+++ install-zscaler-ca.sh
@@ -33,25 +33,12 @@ pB5iDj2mUZH1T8lzYtuZy0ZPirxmtsk3135+CKNa2OCAhhFjE0xd
 EOF
 )
 
-sudo() {
-  [[ "${EUID}" == 0 ]] || set -- command sudo "${@}"
-  # If you're the root user then short circuit out of this OR condition. This
-  # is the Docker use case.
-  #
-  # $EUID is the effective user. Unlike $USER, $EUID should be defined in most
-  # Docker images. 
-  #
-  # Otherwise, take whatever arguments you passed into this function and then
-  # run it through the sudo command. This is the personal machine use case and
-  # is the other side of the OR condition.
-  #
-  # `command` is a bash command to execute or display info about a command and
-  # we're using `set` to modify the args of ${@}.
-
-  "${@}"
-  # Run whatever arguments you passed into this function. This would be whatever
-  # the command was but without `sudo`.
-}
+# Workaround for docker and/or busybox based images that don't have sudo or use bash.
+if [[ "${EUID}" == 0 || -z "${EUID}" ]]; then
+    SUDOCMD=""
+else
+    SUDOCMD=$(which sudo)
+fi
 
 # Source the os release values to determine if we need any packages
 source <(sed 's/ /_/g' /etc/os-release)
@@ -61,39 +48,39 @@ echo "${ZS_PEM}" > /tmp/ZScaler-CA-Chain.pem
 
 echo "Install ZScaler CA Certificate into certificate chain (sudo password may be required)..."
 if [ -f "/usr/sbin/update-ca-certificates" ]; then
-    sudo mv /tmp/ZScaler-CA-Chain.pem /usr/local/share/ca-certificates/ZScaler-CA-Chain.crt
-    sudo /usr/sbin/update-ca-certificates --fresh
+    ${SUDOCMD} mv /tmp/ZScaler-CA-Chain.pem /usr/local/share/ca-certificates/ZScaler-CA-Chain.crt
+    ${SUDOCMD} /usr/sbin/update-ca-certificates --fresh
     echo "done!"
 elif [ -f  "/usr/bin/update-ca-trust" ]; then
-    sudo mv /tmp/ZScaler-CA-Chain.pem /etc/pki/ca-trust/source/anchors
-    sudo /usr/bin/update-ca-trust
+    ${SUDOCMD} mv /tmp/ZScaler-CA-Chain.pem /etc/pki/ca-trust/source/anchors
+    ${SUDOCMD} /usr/bin/update-ca-trust
     echo "done!"
 else
     echo "Certificate management utility not found. Attempting to install using the system package manager..."
 
     # Temporarily add the certificate to the chain manually to ensure our package manager works.
     if [ -f "/etc/ssl/certs/ca-certificates.crt" ]; then
-        sudo cat /tmp/ZScaler-CA-Chain.pem >> /etc/ssl/certs/ca-certificates.crt
+        ${SUDOCMD} cat /tmp/ZScaler-CA-Chain.pem >> /etc/ssl/certs/ca-certificates.crt
     elif [ -f "/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem" ]; then
-        sudo cat /tmp/ZScaler-CA-Chain.pem >> /etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem
+        ${SUDOCMD} cat /tmp/ZScaler-CA-Chain.pem >> /etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem
     fi
 
     if [[ $ID == "alpine" ]]; then
-        sudo apk --no-cache add ca-certificates sudo && \
-        sudo rm -rf /var/cache/apk/*
-        sudo mv /tmp/ZScaler-CA-Chain.pem /usr/local/share/ca-certificates/ZScaler-CA-Chain.crt
-        sudo update-ca-certificates
+        ${SUDOCMD} apk --no-cache add ca-certificates ${SUDOCMD} && \
+        ${SUDOCMD} rm -rf /var/cache/apk/*
+        ${SUDOCMD} mv /tmp/ZScaler-CA-Chain.pem /usr/local/share/ca-certificates/ZScaler-CA-Chain.crt
+        ${SUDOCMD} update-ca-certificates
     elif [[ $ID == "ubuntu" ]]; then
-        sudo apt-get update
-        sudo apt-get install ca-certificates
-        sudo rm -rf /var/lib/apt/lists/*
-        sudo mv /tmp/ZScaler-CA-Chain.pem /usr/local/share/ca-certificates/ZScaler-CA-Chain.crt
-        sudo update-ca-certificates --fresh
+        ${SUDOCMD} apt-get update
+        ${SUDOCMD} apt-get install ca-certificates
+        ${SUDOCMD} rm -rf /var/lib/apt/lists/*
+        ${SUDOCMD} mv /tmp/ZScaler-CA-Chain.pem /usr/local/share/ca-certificates/ZScaler-CA-Chain.crt
+        ${SUDOCMD} update-ca-certificates --fresh
     elif [[ $ID == "fedora" ]]; then 
-        sudo dnf -y install ca-certificates
-        sudo rm -rf /var/cache/libdnf5/*
-        sudo mv /tmp/ZScaler-CA-Chain.pem /etc/pki/ca-trust/source/anchors
-        sudo update-ca-trust
+        ${SUDOCMD} dnf -y install ca-certificates
+        ${SUDOCMD} rm -rf /var/cache/libdnf5/*
+        ${SUDOCMD} mv /tmp/ZScaler-CA-Chain.pem /etc/pki/ca-trust/source/anchors
+        ${SUDOCMD} update-ca-trust
     fi
     echo "done!"
 fi
@@ -102,7 +89,7 @@ fi
 if which systemctl &> /dev/null; then 
     if systemctl is-active --quiet docker; then
         echo -n "Restarting docker..."
-        sudo systemctl restart docker
+        ${SUDOCMD} systemctl restart docker
         echo "done!"
     fi
 fi
